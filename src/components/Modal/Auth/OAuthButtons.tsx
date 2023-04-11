3ced672826b6ae47c56b6e3376498f076def2b4f
Hrithik T <43098636+hrithikt@users.noreply.github.com>
2023-04-11 21:39:44 +0530

    lint error fixes

--- src/components/Modal/Auth/OAuthButtons.tsx
+++ src/components/Modal/Auth/OAuthButtons.tsx
@@ -1,10 +1,10 @@
 import { Button, Flex, Image } from "@chakra-ui/react"
 
-const OAuthButtons = (props: Props) => {
+const OAuthButtons = () => {
     return (
         <Flex direction="column" width="100%" mb={4}>
             <Button variant="oauth" mb={2}>
-                <Image src="/images/googlelogo.png" height="20px" mr={4} />
+                <Image src="/images/googlelogo.png" height="20px" mr={4} alt="Google logo" />
                 Continue with Google
             </Button>
         </Flex>
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -8,9 +8,10 @@ const Navbar = (props: Props) => {
     return (
         <Flex bg="white" height="44px" padding="6px 12px">
             <Flex align="center">
-                <Image src='/images/redditFace.svg' height="30px" />
+                <Image src='/images/redditFace.svg' height="30px" alt='Reddit Logo' />
                 <Image src='/images/redditText.svg' height="46px"
-                    display={{ base: "none", md: "unset" }} />
+                    display={{ base: "none", md: "unset" }}
+                    alt='reddit Text' />
             </Flex>
             <SearchInput />
             {/* <Directory /> */}
